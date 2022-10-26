c3cc76dc91bc5d6086dd68fffca5a00d820c2cf6
Florian Beck <flrn.beck@gmail.com>
2022-10-26 20:16:39 +0200

    Add test for inference

--- starter/starter/ml/tests/test_model.py
+++ starter/starter/ml/tests/test_model.py
@@ -1,15 +1,17 @@
 """ Tests for starter/model.py """
 import numpy as np
 from pytest import approx
+import sklearn
 
 import starter.ml.model as model
-from starter.ml.model import compute_model_metrics
+import starter.ml.data as data
 
 def test_train_model():
     """ Simple smoke test for training a model for binary classification """
     X = np.random.randn(100, 30)
     y = np.random.randint(2, size=100) # binary
     mod = model.train_model(X, y)
+    assert type(mod) == sklearn.linear_model.LogisticRegression # TODO: store type of model e.g. in params?
 
 
 def test_compute_model_metrics():
@@ -26,6 +28,13 @@ def test_compute_model_metrics():
     assert (1/3, 1/4, approx(2/7, abs=1e-10)) == model.compute_model_metrics(y1, y1_pred) 
 
 
-def test_inference():
+def test_inference(test_df):
     """ Test inference """
-    
+    categorical_features = data.get_categorical_features(test_df)
+    X, y, encoder, lb = data.process_data(test_df, categorical_features, "income", training=True)
+
+    mod = model.train_model(X, y)
+    y_pred = model.inference(mod, X)
+
+    assert type(model.inference(mod, X)) == np.ndarray
+    assert model.inference(mod, X).shape == y.shape
