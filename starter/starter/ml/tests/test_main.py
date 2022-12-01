ac0155cd4ec9b69844df5057fc5ce970b378f5ca
Florian Beck <flrn.beck@gmail.com>
2022-12-01 22:06:38 +0100

    Better adapted test

--- starter/starter/ml/tests/test_main.py
+++ starter/starter/ml/tests/test_main.py
@@ -1,3 +1,4 @@
+import copy
 import json
 
 import pydantic
@@ -14,7 +15,7 @@ DATA = {
         "workclass": "Federal-gov",
         "fnlwgt": 8281,
         "education": "Doctorate",
-        "education-num": 891213,
+        "education-num": 8,
         "marital-status": "Divorced",
         "occupation": "Exec-managerial",
         "relationship": "Wife",
@@ -26,17 +27,61 @@ DATA = {
         "native-country": "Jamaica"
     }
 
+DATA2 = {
+        "age": 27,
+        "workclass": "Private",
+        "fnlwgt": 61800,
+        "education": "Bachelors",
+        "education-num": 16,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 10000,
+        "capital-loss": 0,
+        "hours-per-week": 45,
+        "native-country": "United-States"
+}
+
+
 def test_CensusItem():
+    """Just a smoke test
+    TODO: better assert something?
+    """
     ci = CensusItem.parse_obj(DATA)
     print(ci)
     print(ci.dict())
     print(ci.to_dataframe())
 
 
+def test_api_get():
+    r = requests.get("http://127.0.0.1:8000/")
+    assert r.status_code == 200
+    assert r.json() == "Welcome to the ML model server!"
+ 
+
 def test_api_post():
-   r = requests.post("http://127.0.0.1:8000/inference/", json=DATA)
-   print(r.json())
+    r = requests.post("http://127.0.0.1:8000/inference/", json=DATA)
+    assert r.status_code == 200
+    assert r.json() == '[">50K"]'
+
+
+def test_api_post2():
+    # TODO: refactor these two tests?!
+    r = requests.post("http://127.0.0.1:8000/inference/", json=DATA2)
+    assert r.status_code == 200
+    assert r.json() == '[">50K"]'
 
 
 def test_api_post_failure():
-    pass
+    FALSE_DATA = copy.deepcopy(DATA) # deep copy just to be sure (i.e. not side effects on DATA)
+    del FALSE_DATA["age"]
+    FALSE_DATA["agw"] = 53 # add typo
+    r = requests.post("http://127.0.0.1:8000/inference/", json=FALSE_DATA)
+    assert r.status_code == 422
+    FALSE_DATA["age"] = 67 # add another entry
+    r = requests.post("http://127.0.0.1:8000/inference/", json=DATA)
+    assert r.status_code == 200 # ok because all necessary data is provided 
+
+
