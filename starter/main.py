5332175e4dedff5470fba0d3e2458f860d53ec55
Florian Beck <flrn.beck@gmail.com>
2022-12-01 22:07:42 +0100

    Add example

--- starter/main.py
+++ starter/main.py
@@ -1,3 +1,4 @@
+"""Main module to call model inference via FastAPI"""
 import json
 import pandas as pd
 from fastapi import FastAPI
@@ -28,6 +29,27 @@ class CensusItem(BaseModel):
     hours_per_week: int = Field(alias="hours-per-week")
     native_country: str = Field(alias="native-country")
 
+    class Config:
+        """Provide example input"""
+        schema_extra = {
+            "example": {
+                "age": 53,
+                "workclass": "Federal-gov",
+                "fnlwgt": 8281,
+                "education": "Doctorate",
+                "education-num": 8,
+                "marital-status": "Divorced",
+                "occupation": "Exec-managerial",
+                "relationship": "Wife",
+                "race": "Black",
+                "sex": "Female",
+                "capital-gain": 1500,
+                "capital-loss": 500,
+                "hours-per-week": 30,
+                "native-country": "Jamaica",
+            }
+        }
+
     def to_dataframe(self) -> pd.DataFrame:
         # modify dict for converting to dataframe 
         # this avoids the ValueError "... you must pass an index" (alternatively use an index?)
@@ -41,22 +63,13 @@ app = FastAPI()
 
 @app.get("/")
 async def greeting():
-    return {"Welcome to the ML model server!"}
-
+    return "Welcome to the ML model server!"
 
 
 @app.post("/inference/")
 async def model_inf(census_req: CensusItem):
-    # check data
-    # try:
-    #     census_req = CensusItem.parse_obj(census_req)
-    # except Exception as exc:
-    #     raise exc
+    """Call model inference"""
     model = get_model(mode="load")
     census_req_df = census_req.to_dataframe()
     pred = inference_pd(model, census_req_df, decode=True)
     return json.dumps(pred.tolist()) # needed for FastAPI responses (jsonable_encoder)
-    # get encoders -> refactor
-    # encode CensusItem (ordering in BaseModels?)
-    # NOTE: even though we need to convert data the pydantic BaseModel for data checking (no excepts etc.)
-    # return model inference
